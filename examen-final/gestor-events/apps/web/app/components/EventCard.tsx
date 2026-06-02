af7e6db87afc625d0691b3c65bf307067faea3dd
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-06-02 18:16:02 +0100

    B6, B7,B8

--- examen-final/gestor-events/apps/web/app/components/EventCard.tsx
+++ examen-final/gestor-events/apps/web/app/components/EventCard.tsx
@@ -4,7 +4,10 @@ function EventCard({ evento }) {
   const { id, nom, data, ciutat } = evento;
 
   return (
-    <div id={id}>
+    <div
+      id={id}
+      className="border flex, flex-col w-fit p-5 gap-3 rounded hover:bg-amber-500 bg-amber-100"
+    >
       <h2>Nombre: {nom}</h2>
       <p>Fecha: {data}</p>
       <p>Ciudad: {ciutat}</p>
--- examen-final/gestor-events/apps/web/app/events/page.tsx
+++ examen-final/gestor-events/apps/web/app/events/page.tsx
@@ -3,9 +3,9 @@ import EventCard from "../components/EventCard";
 
 export default function Events() {
   return (
-    <div>
-      <h1>Página eventos</h1>
-      <main>
+    <div className="py-10 bg-blue-100 min-h-screen">
+      <h1 className="text-center mb-20 text-5xl">Página eventos</h1>
+      <main className="flex justify-center items-center gap-3">
         {eventos.map((e) => (
           <EventCard key={e.id} evento={e}></EventCard>
         ))}
